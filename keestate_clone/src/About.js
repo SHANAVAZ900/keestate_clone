2356a2f7f01fd3b28290b62739a0569eb680052e
zamindar-786 <zamindar900@gmail.com>
2021-03-09 00:05:45 +0530

    done

--- keestate_clone/src/About.js
+++ keestate_clone/src/About.js
@@ -11,19 +11,20 @@ function About() {
                 </h1>
                 <p>
                     We are an early-stage education-tech startup founded in 2020.
-                    We are building a college social platform that focuses on the gamification educationin college.
+                    We are building a college social platform that focuses on the gamification education in college.
                 </p>
             </div>     
             <div className="about__team">
-                <h1>Founding Team</h1>
-                <h2>Joel Vinay Kumar, co-founder(CTO)</h2>
+                <h1>CTO</h1>
+                <h2>Joel Vinay Kumar</h2>
                 <p>
                     Post-graduation from IIITH in 2019 and have 2 years of experience in building SaaS products.
 
                 </p>
             </div>
             <div className="about__team">
-                <h2>Thakur Ujwal Singh, co-founder(CBO)</h2>
+                <h1>CBO</h1>
+                <h2>Thakur Ujwal Singh</h2>
                 <p>
                     MBA from IIM Sirmaur in 2020 and have 3 years of experience in ed-tech
                     sector and e-commerce.
@@ -31,7 +32,8 @@ function About() {
                 </p>
             </div>
             <div className="about__team">
-                <h2>Roopesh P, founding team(CMO)</h2>
+                <h1>CMO</h1>
+                <h2>Roopesh P</h2>
                 <p>
                      MBA from IIM Sirmaur in 2020, B.Tech from SRM University 2012 and have 3
                     years of experience in the staffing industry and ed-tech.

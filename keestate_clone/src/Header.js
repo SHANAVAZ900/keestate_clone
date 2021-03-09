c179b08ee1d2fdb05bec9727f6172cdb55522387
zamindar-786 <zamindar900@gmail.com>
2021-03-09 15:16:01 +0530

    done

--- keestate_clone/src/Header.js
+++ keestate_clone/src/Header.js
@@ -1,22 +1,36 @@
 import React from 'react'
 import './Header.css'
 import { } from "@material-ui/core"
+import { Link } from "react-router-dom"
+import "./Form"
 
 
 function Header() {
     return (
         <div className="header">
-            <img className="header__icon"
-            src="https://keestep.com/static/media/logo.b82f7861.png" alt="" />
+            <Link to="/">
+                <img className="header__icon"
+                src="https://keestep.com/static/media/logo.b82f7861.png" alt="" />
+            </Link>
+            
+    
+            
 
             <div className="header__center">
 
             </div>
 
             <div className="header__right">
-                <p>Home</p>
-                <p>About</p>
-                <p>Contact Us</p>
+                <Link to ="/" style={{ textDecoration: 'none',hover:{color:'rgb(218, 73, 29)'}}}>
+                    <p>Home</p>
+                </Link>
+                <Link to="/about" style={{ textDecoration: 'none'}}>
+                    <p>About</p>
+                </Link>
+                <Link to="/" style={{ textDecoration: 'none'}}>
+                    <p>Contact Us</p>
+                </Link>
+                
                
             </div>
 

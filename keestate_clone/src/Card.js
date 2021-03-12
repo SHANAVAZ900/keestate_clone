6f584f985ef27c7b92fc6247ec2fcaed5f21c28a
zamindar-786 <zamindar900@gmail.com>
2021-03-12 23:05:05 +0530

    done changes in card and home

--- keestate_clone/src/Card.js
+++ keestate_clone/src/Card.js
@@ -4,9 +4,9 @@ import "./Card.css"
 
 function Card({ src, title}) {
     return (
-        <div className="card">
+        <div className="card" variant="outlined">
             <img src={src} alt=""/>
-            <div className="card__info">
+            <div className="card__info" variant="outlined">
                 <Button>{title}</Button>
             </div>
         </div>
--- keestate_clone/src/Home.js
+++ keestate_clone/src/Home.js
@@ -14,6 +14,8 @@ function Home() {
             <div className="home__section">
                 <Card src="https://keestep.com/static/media/discussion.d31f13e2.png" title="Book a Demo"/>
                 <Card src="https://www.growthengineering.co.uk/wp-content/uploads/2014/07/gamify-your-goals.jpg" title="Book a Demo"/>
+                <Card src="https://pbs.twimg.com/media/EcUkSurU0AAvUk6.jpg" title="Book a Demo"/>
+                <Card src="https://qwasar.io/wp-content/uploads/2020/07/Gamification-in-Learning-1.png" title="Book a Demo"/>
 
             </div>
 
